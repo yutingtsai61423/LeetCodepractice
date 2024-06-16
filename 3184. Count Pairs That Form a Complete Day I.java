317f03e9c2823d939add6e423187fe6c652734c8
Vicky Tsai <111208806+yutingtsai61423@users.noreply.github.com>
2024-06-16 15:13:44 +0800

    Create 3184. Count Pairs That Form a Complete Day I.java

---
+++ 3184. Count Pairs That Form a Complete Day I.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countCompleteDayPairs(int[] hours) {
+        int result = 0;
+        
+        for (int i = 0; i < hours.length -1 ; i++){
+            for ( int j = i + 1; j < hours.length; j++){
+                if ((hours[i] + hours[j]) % 24 == 0 ){
+                    result++;
+                }
+            }
+        }
+        return result;        
+        
+    }
+}
